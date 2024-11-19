e4bb9f704b4bb0016f5eb6a00a8f8344f92d2491
Simeon Ng <simeon.ng@alaska.gov>
2024-11-19 11:56:10 -0900

    Update query_agileweb
    Can now get > 10k records from a single query

--- R/agileweb.R
+++ R/agileweb.R
@@ -6,8 +6,8 @@
 
 #' Query AMQA internal agileweb server for site/parameter data.
 #'
-#' @param sites Site name string. Optionally a comma-separated list of site names. It is recommended to provide only one site name in a query, as the API is limited to 10,000 records at once (Site names are identical as listed in AirVision > Configuration Editors > Site/Parameter).
-#' @param parameters Parameter string. Optionally a comma-separated list of parameter names. (Parameter names are identical as listed in AirVision > Configuration Editors > Site/Parameters)
+#' @param sites Site name string. Optionally a comma-separated list of site names (Site names are identical as listed in AirVision > Configuration Editors > Site/Parameter).
+#' @param parameters Parameter string. Optionally a comma-separated list of parameter names (Parameter names are identical as listed in AirVision > Configuration Editors > Site/Parameters).
 #' @param interval Interval string. Defaults to 001h.
 #' @param start Start date/time in format yyyy-mm-ddTHH:mm:ss
 #' @param end End date/time in format yyyy-mm-ddTHH:mm:ss. Defaults to current time.
@@ -16,34 +16,57 @@
 #' @return A tibble with selected columns.
 #' @export
 query_agileweb <- function(sites, parameters, interval = NULL, start, end = NULL, api_key) {
+  # Default 1 hour interval
   if(is.null(interval)) {
     interval <- "001h"
   }
+  # Default end date is midnight of present day.
   if(is.null(end)) {
     end <- agileweb_dt(lubridate::now())
   }
-  req <- httr2::request(
-    paste0(
-      "https://dec.alaska.gov/applications/air/airvision/api/averagedata?",
-      "sites=", sites, "&",
-      "parameters=", parameters, "&",
-      "interval=", interval, "&",
-      "start=", start, "&",
-      "end=", end
-    )
-  ) |>
-    httr2::req_headers("APIKEY" = api_key, .redact = "Secret") |>
-    httr2::req_headers("Accept" = "text/csv")
-  req
-  resp <- httr2::req_perform(req) |>
-    httr2::resp_body_string() |>
-    readr::read_csv(show_col_types = FALSE)
-  return(
-    resp |>
-      dplyr::select(
-        "Date", "SiteName", "ParameterName", "ReportedUnitName", "IntervalName", "IsValid", "ReportValue", "FlagString"
+  # Default end_date_data is present datetime
+  end_date_query <- lubridate::parse_date_time(end, "ymd HMS")
+  end_date_data <- lubridate::parse_date_time(start, "ymd HMS")
+  new_start_date <- start
+  data <- NULL
+  # Loop to keep querying agileweb when there are > 10k records in the date range.
+  while(end_date_data < end_date_query) {
+    req <- httr2::request(
+      paste0(
+        "https://dec.alaska.gov/applications/air/airvision/api/averagedata?",
+        "sites=", stringr::str_replace(sites, " ", "%20"), "&",
+        "parameters=", parameters, "&",
+        "interval=", interval, "&",
+        "start=", new_start_date, "&",
+        "end=", end
       )
-  )
+    ) |>
+      httr2::req_headers("APIKEY" = api_key, .redact = "Secret") |>
+      httr2::req_headers("Accept" = "text/csv")
+    req
+    resp <- httr2::req_perform(req) |>
+      httr2::resp_body_string() |>
+      readr::read_csv(show_col_types = FALSE) |>
+      dplyr::select(
+          "Date",
+          "SiteName",
+          "ParameterName",
+          "ReportedUnitName",
+          "IntervalName",
+          "IsValid",
+          "ReportValue",
+          "FlagString"
+      )
+    data <- dplyr::bind_rows(data, resp)
+    end_date_data <- max(data$Date)
+    new_start_date <- agileweb_dt(max(data$Date))
+    test_length <- end_date_query - end_date_data
+    # Stop querying if data within 2 hours of query end.
+    if(time_length(test_length, unit = "hour") < 3) {
+      break
+    }
+  }
+  return(data)
 }
 
 #' agileweb_dt
