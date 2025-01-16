85e89531e6eca6ca3660d98223f43da6a43e1864
Simeon Ng <simeon.ng@alaska.gov>
2025-01-16 11:05:28 -0900

    Add linear regression function

--- R/linear.R
+++ R/linear.R
@@ -1,3 +1,25 @@
+#' Linear Regression Analysis
+#'
+#' @param data Data to analyze
+#' @param x String of column of independent variable
+#' @param y String of column of dependent variable
+#'
+#' @return Vector of lm coefficients
+#' @export
+#'
+#' @examples
 linear_regression <- function(data, x, y) {
+  mod <- parsnip::linear_reg()
+  fit <- mod |>
+    parsnip::fit(.data[[y]] ~ .data[[x]], data = data)
 
+  fit_tidy <- parsnip::tidy(fit)
+  fit_glance <- parsnip::glance(fit)
+
+  fit_int   <- fit_tidy$estimate[1]
+  fit_slope <- fit_tidy$estimate[2]
+  fit_r2    <- fit_glance$r.squared
+  fit_rmse  <- yardstick::rmse(data, .data[[x]], .data[[y]])
+
+  return(c(fit_slope, fit_int, fit_r2, fit_rmse))
 }
