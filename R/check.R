cd047fef863b2b53c064f0a60fb2ff8460b36e77
Ng <simeon.ng@alaska.gov>
2025-02-04 14:27:09 -0900

    Add check.R

---
+++ R/check.R
@@ -0,0 +1,10 @@
+#' Check Completeness of Dataset
+#'
+#' @param data AQ dataset
+#' @param pollutant Pollutant to analyze completeness
+#'
+#' @return Boolean of if data meets completeness
+#' @export
+check_completeness <- function(data, pollutant) {
+  # TODO
+}
