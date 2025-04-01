1c1f84e297a4aa648da9e9a481f689da31059e2d
Simeon Ng <simeon.ng@alaska.gov>
2025-04-01 08:37:07 -0800

    Add valid nowcast function

---
+++ R/nowcast.R
@@ -0,0 +1,28 @@
+# nowcast.R
+# Calculation of the PM NowCast API as described in the EPA Technical Asssistance Document
+# https://document.airnow.gov/technical-assistance-document-for-the-reporting-of-daily-air-quailty.pdf
+
+#' nowcast
+#'
+#' @param data PM Dataset
+#' @param pm_col PM Column Name
+#'
+#' @return
+#' @export
+nowcast <- function(data) {
+
+  # 1. Select minimum and maximum PM measurements
+
+}
+
+#' Valid NowCast hour if 2 out of the last 3 hours of data are valid.
+#'
+#' @noRd
+valid_nowcast <- function(data) {
+  if(sum(is.na(data)) > length(data) / 3) {
+    return(FALSE)
+  }
+  else {
+    return(TRUE)
+  }
+}
