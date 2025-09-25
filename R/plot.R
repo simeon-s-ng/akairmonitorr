92e46dfed330cf76e7b6a3c2a232b4b754491310
Ng <simeon.ng@alaska.gov>
2025-09-25 14:38:41 -0800

    Update cc plot

--- R/plot.R
+++ R/plot.R
@@ -530,7 +530,7 @@ plot_diurnal_cc <- function(data, site, start, end, statistic, title) {
     ggplot2::ggplot(ggplot2::aes(date, pm25, color = site)) +
       ggplot2::geom_line(linewidth = 1, lineend = "round") +
       ggplot2::guides(fill = 'none') +
-      ggplot2::scale_x_datetime(date_labels = "%b") +
+      ggplot2::scale_x_datetime(date_labels = "%b", date_breaks = "1 month") +
       ggplot2::scale_y_continuous(
         expression("PM"["2.5"] ~ "(\u03bcg/m\u00b3)"),
       ) +
